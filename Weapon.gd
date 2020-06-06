c21319bb856c023b42e606b90b079346211cdfc8
burnsedia <bburnsed1995@gmail.com>
2020-06-06 02:32:39 -0400

    I hope this works

---
+++ Weapon.gd
@@ -0,0 +1,30 @@
+extends Area
+class_name weapon
+
+var damage = 10
+var shot_timer = Timer.new()
+var RoF = 10
+var player_owned = false
+
+export(PackedScene) var bulit
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func shoot():
+	var projectile = bulit.instance()
+	add_child(projectile)
+		
+	
+		
+		
+	
+		
