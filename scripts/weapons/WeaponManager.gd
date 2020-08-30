47f04e8cd5b68af3f03ba3a87cf63918e1ff32d8
burnsedia <bburnsed1995@gmail.com>
2020-08-30 09:37:43 -0400

    tweeking the weapon systems

--- scripts/weapons/WeaponManager.gd
+++ scripts/weapons/WeaponManager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Spatial
 
 
 # Declare member variables here. Examples:
@@ -12,8 +12,25 @@ var manager = get_parent()
 func _ready():
 	pass # Replace with function body.
 
+func _init():
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func Fire(cur_weapon: weapon):
+	if cur_weapon.has_method("use"):
+		cur_weapon.get_cur_weapon().use()
+		
 
+func set_cur_weapon():
+	pass
+
+func get_cur_weapon(cur_weapon: weapon):
+	for wn in weapons:
+		if wn == weapon:
+			wn = cur_weapon
+		return cur_weapon
+	
+func get_avaible_weapons():
+	pass
+
+func cur_weapon_is_overheated(heatLimate: int):
+	pass
