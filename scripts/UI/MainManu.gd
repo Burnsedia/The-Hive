c0baedd535df0ef8ce93258891442d29eae70c3c
burnsedia <bburnsed1995@gmail.com>
2020-10-01 20:18:29 -0400

    Makng the MainManu

---
+++ scripts/UI/MainManu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+onready var NewGame = $menu/Hb/cc/Butons/NewGame
+onready var LoadGame = $menu/Hb/cc/Butons/LoadGame
+onready var Options = $menu/Hb/cc/Butons/Options
+onready var Settings = $menu/Hb/cc/Butons/Settings
+onready var Exit = $menu/Hb/cc/Butons/Exit
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_NewGame_pressed():
+	get_tree().change_scene("res://Sceens/Game_Root.tscn")
+
+func _on_Exit_pressed():
+	get_tree().quit()
