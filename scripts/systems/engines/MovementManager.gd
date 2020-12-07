8a710ea832157fb4f39b3c61467e4f7f65c358d5
Burnsedia <bburnsed1995@gmail.com>
2020-12-06 22:17:30 -0500

    reworking Movement to be more modular and work with multiplayer

--- scripts/systems/engines/MovementManager.gd
+++ scripts/systems/engines/MovementManager.gd
@@ -1,18 +1,26 @@
 extends Spatial
 
 export var BodyToMove := KinimaticBody
-export var = 500
+export var MoveSpeed = 500.0
 export var MaxSpeed = 1000.0
 export var TurnSpeed = 2.0
-export var BootSpeedModifer = 20.0
+export var BoostSpeedModifer = 20.0
+
+# class properties
+var MoveVector = Vector3()
+var CanBoost = false
 
 
 
-func init():
-    pass
+sync func init():
+	pass
+	
 
 func _process(delta):
 	pass
 
 func _physics_process(delta):
-    pass
+	pass
+
+
+	
