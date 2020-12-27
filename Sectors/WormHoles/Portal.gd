f0e435b4c30a9cea332bd7245aa3cedf82e956ec
Burnsedia <bburnsed1995@gmail.com>
2020-12-27 03:19:04 -0500

    working on portals between sectors, area not detecting player body entering

--- Sectors/WormHoles/Portal.gd
+++ Sectors/WormHoles/Portal.gd
@@ -4,15 +4,14 @@ class_name Portal
 export(PackedScene) var ConnectedSector 
 
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_Portal_body_entered(body):
-	get_tree().change_scene_to(ConnectedSector)
+	print(body.name + " entered bortal")
+	print(body.val)
+	ConnectedSector.instance()
+	Globals.current_scene = ConnectedSector
+	body.queue_free()
+	Globals.switch_sceen(ConnectedSector)
+
+
+
+	
