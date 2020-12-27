0fff8e6abbb9decb0fdbe683ac4311fb419bc019
Burnsedia <bburnsed1995@gmail.com>
2020-12-27 03:20:35 -0500

    made the drones use movement manager

--- Units/units/Military/HiveSwarmShip.gd
+++ Units/units/Military/HiveSwarmShip.gd
@@ -21,64 +21,52 @@ var turn_speed = 10
 
 
 
-func _ready():
+func _ready() -> void:
 	timer.start()
 	add_to_group("hive")
 	get_tree().call_group("player", "_I_am_the_player")
  
-func _process(delta):
+func _process(delta) -> void:
 	set_player_vec()
 	Movement.set_move_vec(mov_vec)
 	
 	
-func attack():
+func attack() -> void:
 	if tgarget.get_collider() == player:
 		weapon.use(self)
 		can_shoot = false
 		timer.start()
 		
-func kill():
+func kill() -> void:
 	dead = true
 	$CollisionShape.disabled = true
 	queue_free()
  
-func set_player(p):
+func set_player(p) -> void:
 	player = p
-	print("player set")
+#	print("player is set")
 
-func set_player_vec():
-	if player == null:
-		print("Player not Set, can kill you today")
-		can_shoot = false
-		return
-	var player_vec = transform.origin - player.transform.origin
-	mov_vec =  -player_vec
+func set_player_vec() -> Vector3:
+	var player_vec = player.transform.origin - self.transform.origin
+	mov_vec =  player_vec
 	
 	return mov_vec
 
 #func aim():
 #	var disired_rotation = transform.looking_at(player.transform.origin, Vector3.UP)
 #	var a = Quat(transform.basis.get_rotation_quat().slerp(disired_rotation.get_rotation_quat())
-
+#	# need too finish this
+#	pass
 
 # check if NPC needs to avoid closion
-#func avoid_collion():
-#	mov_vec = transform.basis.z * 50
-#	if NavRight.is_colliding():
-#		rotate(Vector3.UP, .05)
-#	elif NavLeft.is_colliding():
-#		rotate(Vector3.UP, .05)
+#func avoid() -> Vector3:
+#	pass
 #
-#	if NavUp.is_colliding():
-#		rotate(Vector3.RIGHT, .05)
-#	elif NavDown.is_colliding():
-#		rotate(Vector3.RIGHT, -.05)
-	
-func _on_ShootTimer_timeout():
+func _on_ShootTimer_timeout() -> void:
 	can_shoot = true
 	attack()
 
-func damage(damage):
+func damage(damage) -> void:
 	print("hit by")
 	hp -= damage
 	if hp <= 0:
