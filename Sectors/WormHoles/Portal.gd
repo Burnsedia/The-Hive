1f74a7873ab0e3c4588ee0261aadc5b46768b851
Overlord <overlord@pop-os.localdomain>
2021-01-06 20:57:38 -0500

    working on making the galaxy map system

--- Sectors/WormHoles/Portal.gd
+++ Sectors/WormHoles/Portal.gd
@@ -2,12 +2,14 @@ extends Area
 class_name Portal
 
 export(PackedScene) var ConnectedSector 
+onready var _sector = get_parent()
 
 # sent body to next sector
-func _on_Portal_body_entered(body:KinimaticBody) -> void:
+func _on_Portal_body_entered(body) -> void:
 	print(body.name + " entered bortal")
 	print(body.val)
 	get_tree().change_scene_to(ConnectedSector)
 	Globals.current_scene = ConnectedSector
-	body.queue_free()
+	#body.queue_free()
+	emit_signal("JumpedToSector", _sector, ConnectedSector)
 	
