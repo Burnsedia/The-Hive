155e4bb6a2db50817fd6137d6c75f538dfe0836f
Overlord <overlord@pop-os.localdomain>
2021-01-13 19:12:30 -0500

    working on the jump system

--- Sectors/WormHoles/Portal.gd
+++ Sectors/WormHoles/Portal.gd
@@ -1,13 +1,14 @@
 extends Area
 class_name Portal
 
+
 export(PackedScene) var ConnectedSector 
 
 # sent body to next sector
-func _on_Portal_body_entered(body:KinimaticBody) -> void:
+func _on_Portal_body_entered(body:KinematicBody) -> void:
 	print(body.name + " entered bortal")
 	print(body.val)
 	get_tree().change_scene_to(ConnectedSector)
 	Globals.current_scene = ConnectedSector
-	body.queue_free()
+	
 	
