1b74a431b39f9fea7fbba67f30a4142e6bd5a990
Burnsedia <bburnsed1995@gmail.com>
2021-10-07 16:49:53 -0400

    working on mainmainue for starting sector

--- UI/MainManu.gd
+++ UI/MainManu.gd
@@ -11,7 +11,7 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_NewGame_pressed():
-	get_tree().change_scene("res://Sceens/Game_Root.tscn")
+	get_tree().change_scene("res://Sceens/StartSector.tscn")
 	print("Starting new game")
 
 func _on_Exit_pressed():
