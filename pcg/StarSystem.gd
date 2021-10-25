123b09871b7dde17312cb526025cdca8145fea76
Burnsedia <bburnsed1995@gmail.com>
2021-10-24 21:52:14 -0400

    added a reouse file for eas of design

---
+++ pcg/StarSystem.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name StarSystem
+
+export(Image) var starIcon = null
+export(bool) var clammed = false
+export(int) var numPlnaets = 6
+export(int) var numStations = 36
+export(String) var name = "PX-335" 
